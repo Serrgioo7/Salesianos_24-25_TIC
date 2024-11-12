57ba77f6e0ec65ecb968a27fffaaa85703645e6e
Serrgioo7 <lorente.seser18@zaragoza.salesianos.edu>
2024-11-12 10:03:09 +0100

    Update Adivina.py

--- Adivina.py
+++ Adivina.py
@@ -1,14 +1,26 @@
 #Un programa en el que el usuario tiene que adivinar
-#el número que el ordenador ha elegido al azar. El programa
+#el nÃºmero que el ordenador ha elegido al azar. El programa
 #va respondiendo mayor o menor cuando el usuario le va
-#proponiendo números
+#proponiendo nÃºmeros
 import random
 import time
 
 def adivina():
-    print("Voy a elegir al azar un número del 1 al 10 y tú tienes que adivinarlo.")
-    seguir=raw_input("¿Estás preparado (S/N)?")
+    print("Voy a elegir al azar un nÃºmero del 1 al 10 y tÃº tienes que adivinarlo.")
+    seguir=raw_input("Â¿EstÃ¡s preparado (S/N)?")
     semilla=time.time()
- 
-adivina()
+    random.seed(semilla)
+    numero=int(random.random()*10)%10+1
+    print("NUMERO: "+str(numero))
+    while(seguir=='S'):
+        respuesta=input("Â¿QuÃ© nÃºmero he pensado? (1-10) ")
+        if(respuesta<numero):
+            print("MAYOR")
+        if(respuesta>numero):
+            print("MENOR")
+        if(respuesta==numero):
+            print("HAS ACERTADO")
+            seguir='N'
+    print("SE ACABÃ“ EL JUEGO")
 
+adivina()
