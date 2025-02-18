9b0f8b4750555f008ea507365e93776fe13497c1
Serrgioo7 <lorente.seser18@zaragoza.salesianos.edu>
2025-02-18 10:07:52 +0100

    Delete Usuario.py

--- Usuario.py
+++
@@ -1,9 +0,0 @@
-def usuario():
-    nombre=raw_input("Nombre: ")
-    apellido1=raw_input("Primer apellido: ")
-    apellido2=raw_input("Segundo apellido: ")
-    ano=raw_input("Año de nacimiento: ")
-    usuario=apellido1[0:3]+apellido2[0:3]+nombre[0:3]+ano[2:4]
-    print("USUARIO:" + usuario+'@gmail.com')
-
-usuario()
